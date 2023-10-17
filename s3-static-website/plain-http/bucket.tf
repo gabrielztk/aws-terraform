649fc73b0cdaede7f02120c97d66363aa6390e4e
Gabriel Zanetti <38334108+gabrielztk@users.noreply.github.com>
2023-10-17 17:42:12 -0300

    Remove ACL & Ownership resources

--- s3-static-website/plain-http/bucket.tf
+++ s3-static-website/plain-http/bucket.tf
@@ -1,36 +1,17 @@
 resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
+  force_destroy = true
 
   tags = {
-    Name        = "Static-Website"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+    Name        = var.bucket_name
+    Environment = terraform.workspace
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
-  ]
-
-  bucket = aws_s3_bucket.example.id
-  acl    = "public-read"
+  block_public_policy = false
 }
 
 resource "aws_s3_bucket_policy" "allow_public_access" {
@@ -49,59 +30,18 @@ data "aws_iam_policy_document" "allow_public_access_policy" {
       type        = "AWS"
     }
     resources = [
-      "arn:aws:s3:::${var.bucket_name}/*",
-      "arn:aws:s3:::${var.bucket_name}"
+      "${aws_s3_bucket.example.arn}/*"
     ]
   }
 }
 
-resource "aws_s3_object" "example-index" {
-  depends_on = [
-    aws_s3_bucket_policy.allow_public_access,
-  ]
 
-  bucket       = aws_s3_bucket.example.id
-  key          = var.index_file
-  source       = "${var.website_root_folder}/${var.index_file}"
-  acl          = "public-read"
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "about-index" {
-  depends_on = [
-    aws_s3_bucket_policy.allow_public_access,
-  ]
-
-  bucket       = aws_s3_bucket.example.id
-  key          = var.about_file
-  source       = "${var.website_root_folder}/${var.about_file}"
-  acl          = "public-read"
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "example-error" {
-  depends_on = [
-    aws_s3_bucket_policy.allow_public_access,
-  ]
-
-  bucket       = aws_s3_bucket.example.id
-  key          = var.error_file
-  source       = "${var.website_root_folder}/${var.error_file}"
-  acl          = "public-read"
-  content_type = "text/html"
-}
 
 resource "aws_s3_bucket_website_configuration" "example" {
   bucket = aws_s3_bucket.example.id
 
-  depends_on = [
-    aws_s3_object.example-index,
-    aws_s3_object.example-error,
-
-  ]
-
   index_document {
-    suffix = var.index_file
+    suffix = var.index_file_sufix
   }
 
   error_document {
